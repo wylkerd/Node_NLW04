bb6fdeebe4db070da3dfe515ab2879b8f9311d36
Wylkerd <wylkerd@hotmail.com>
2021-02-23 10:54:10 -0300

    Created repository, and users exists verification

--- ApiNode/src/controllers/UserController.ts
+++ ApiNode/src/controllers/UserController.ts
@@ -1,10 +1,32 @@
 import { Request, Response } from 'express'
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
 
 class UserController {
     async create(request: Request, response: Response) {
-        const body = request.body;
-        console.log(body);
-        return response.send();
+        // desestruturação do ES6
+        const { name, email } = request.body;
+
+        const usersRepository = getRepository(User);
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        })
+
+        if (userAlreadyExists) {
+            return response.status(400).json({
+                error: "User already exists"
+            });
+        }
+
+        const user = usersRepository.create({
+            name, email
+        })
+
+        // await pois retorna uma promise
+        await usersRepository.save(user);
+
+        return response.json(user);
     }
 };
 
