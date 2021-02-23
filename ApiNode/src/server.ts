c93a3fa4df990165f0b2799f186e3241ec9fbcf4
Wylkerd <wylkerd@hotmail.com>
2021-02-23 10:15:48 -0300

    Created controller, router and refactoring the project

--- ApiNode/src/server.ts
+++ ApiNode/src/server.ts
@@ -1,27 +1,11 @@
 import 'reflect-metadata'
 import express, { request, response } from 'express';
 import './database'
+import { router } from './routes';
 
 const app = express();
 
-/**
- * Get => Buscar
- * Post => Salvar
- * Put => Alterar
- * Delete => Deletar
- * Patch => Alteração especifica
- */
-
-// 1º param => Rota(recurso API)
-// 2º param => request,response
-
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello world - NLW04" });
-});
-
-app.post("/", (request, response) => {
-    // retornou os dados para salvar
-    return response.json({ message: "Os dados foram salvos com sucesso!" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is runing!"));
