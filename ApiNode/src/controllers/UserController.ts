b5c206a185e7230a619000a16ccb0a2c71a584db
Wylkerd <wylkerd@hotmail.com>
2021-02-26 11:15:36 -0300

    User validation

--- ApiNode/src/controllers/UserController.ts
+++ ApiNode/src/controllers/UserController.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express'
 import { getCustomRepository, getRepository } from 'typeorm';
 import { UserRepository } from '../repositories/UserRepository';
+import * as yup from 'yup';
 
 class UserController {
     async create(request: Request, response: Response) {
         // desestruturação do ES6
         const { name, email } = request.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required("Nome é obrigatorio"),
+            email: yup.string().email().required("Email é obrigatorio")
+        })
+
+        try {
+            await schema.validate(request.body, { abortEarly: false })
+        } catch (err) {
+            return response.status(400).json({ error: err })
+        }
 
         const usersRepository = getCustomRepository(UserRepository);
 
